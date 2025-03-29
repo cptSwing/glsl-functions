29a93347a740f5c0dfd6a83e90d4740ed767e484
Jens Brandenburg <jens@vragments.com>
2025-03-29 17:16:36 +0100

    renamed to PS filter standard

--- fragment/posterize.glsl
+++ fragment/posterize.glsl
@@ -1,15 +1,15 @@
-float sliced(float f, float steps) {
+float posterize(float f, float steps) {
     return floor(f * steps) / steps;
 }
 
-vec2 sliced(vec2 v2, float steps) {
+vec2 posterize(vec2 v2, float steps) {
     return floor(v2 * steps) / steps;
 }
 
-vec3 sliced(vec3 v3, float steps) {
+vec3 posterize(vec3 v3, float steps) {
     return floor(v3 * steps) / steps;
 }
 
-vec4 sliced(vec4 v4, float steps) {
+vec4 posterize(vec4 v4, float steps) {
     return floor(v4 * steps) / steps;
 }
